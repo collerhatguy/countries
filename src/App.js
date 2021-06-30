18fb6aba743449975a9b013cb6f66924fbc33582
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-06-30 17:11:58 -0400

    added api and styles reset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-
+import {useState, useEffect} from "react";
+import "./style/style.css";
 
 function App() {
+  const url = "https://restcountries.eu/rest/v2/all";
+  const [data, setData] = useState()
+  async function getData() {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json)
+      console.log(json)
+    } catch(err) {
+      throw err;
+    }
+  }
+  useEffect(() => {
+    getData()
+  },[])
   return (
     <div className="App">
     </div>
