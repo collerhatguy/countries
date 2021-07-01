d046d79693f632fa69bc17545ec0adc137760d7f
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-07-01 16:33:30 -0400

    added title and updated url link

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
       <Router>
         <Switch>
           <Route 
-            path="/" 
+            path="" 
             exact
             component={() =>
               <MainPage
